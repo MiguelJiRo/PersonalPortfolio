83f3466974c5734e636c01bb808c51cbbcc0c020
Miguel Jiménez Rodríguez <migueljiroz@gmail.com>
2022-05-28 19:53:21 +0200

    routes updated

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-// Modulos
 import { PagesRoutingModule } from './pages/pages.routing';
-import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
 
 
 const routes: Routes = [
-    { path: '', redirectTo: '' }
+    { path: '', redirectTo: '', pathMatch: 'full' }
 ];
-
-
 
 @NgModule({
     imports: [
